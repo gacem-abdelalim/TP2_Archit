6009be7730bc8832a2163a375403979194499264
gacem-abdelalim <96735796+gacem-abdelalim@users.noreply.github.com>
2022-12-02 09:05:52 +0100

    Reponse de question 7 et 8

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,13 +1,25 @@
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		IEtudiantRepo etudRepo = new EtudiantRepository();
-		IUnivRepo univRepo = new UniversiteRepository();
+		IJournal journal1 = new JournalDateClass();
+		IJournal journal2 = new JournalEcran();
+		IJournal journal3 = new JournalFile();
+		
+		List<IJournal> listJournal =new ArrayList<IJournal>();
+		listJournal.add(journal2);
+		listJournal.add(journal1);
+		listJournal.add(journal3);
+		IJournal affiche = new AfficheMessage(listJournal);
+		IEtudiantRepo etudRepo = new EtudiantRepository(affiche);
+		IUnivRepo univRepo = new UniversiteRepository(affiche);
+		
 
-		EtudiantService serv= new EtudiantService(etudRepo, univRepo);
+		EtudiantService serv= new EtudiantService(etudRepo, univRepo, affiche);
 		try {
 			serv.inscription(2, "alim", "gacem", "guene@gmail.com","xxxx", 123);
 			
@@ -15,6 +27,6 @@ public class MainApp {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
-
 }
--- src/UniversiteRepository.java
+++ src/UniversiteRepository.java
@@ -7,13 +7,18 @@ import java.sql.Statement;
 
 public class UniversiteRepository implements IUnivRepo{
 	private IDBConnection BD;
+	IJournal affiche;
+	public UniversiteRepository(IJournal affiche) {
+		this.affiche = affiche;
+	}
+	
 	@Override
 	public Universite GetById(int universityId) throws SQLException {
 		
 		BD= (DBConnection) DBConnection.getDBConnection();
 		Connection connect=BD.getConn();
 		Statement stmt = connect.createStatement();
-		System.out.println("LogBD : d�but recherche de id universit� dans la base de donn�e");
+		affiche.outPut_Msg("LogBD : d�but recherche de id universit� dans la base de donn�e");
 		
 		String sql = "select * from universite where id_universite="+ universityId;
 		ResultSet rs = stmt.executeQuery(sql);
@@ -21,7 +26,7 @@ public class UniversiteRepository implements IUnivRepo{
 		TypePackage p=TypePackage.valueOf(rs.getString(3));
 		Universite u = new Universite (rs.getInt(1),rs.getString(2),p);
 			
-		System.out.println("LogBD : universit� r�cup�r�e");
+		affiche.outPut_Msg("LogBD : universit� r�cup�r�e");
 		
 		connect.close();
 		return u;	
